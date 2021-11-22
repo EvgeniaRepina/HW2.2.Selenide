51502f209dad69bc7edab40f7acd262ed8377a07
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-11-22 14:48:33 +0100

    add assertions, add task dropdown list

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,17 +1,18 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.hidden;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryTest {
-
 
     @Test
     void shouldBookingACard() throws InterruptedException {
@@ -28,7 +29,12 @@ class CardDeliveryTest {
 
 
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible);
-        $(byText("Встреча успешно забронирована на " + text)).exists();
+//        или
+//        Duration.ofSeconds(15);
+//        Assertions.assertTrue($(withText("Успешно!")).exists());
+
+        Assertions.assertTrue($(withText("Встреча успешно забронирована на")).exists());
+        Assertions.assertTrue($(By.cssSelector(".notification__content")).shouldHave(Condition.text(text)).exists());
     }
+
 }
